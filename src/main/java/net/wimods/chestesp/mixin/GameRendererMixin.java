25e6b448c73b2b41f6f609a9a89164c62af97e0f
Alexander01998 <contact.wurstimperium@gmail.com>
2024-05-27 14:29:37 +0200

    Update to 1.20.5

--- src/main/java/net/wimods/chestesp/mixin/GameRendererMixin.java
+++ src/main/java/net/wimods/chestesp/mixin/GameRendererMixin.java
@@ -7,30 +7,36 @@
  */
 package net.wimods.chestesp.mixin;
 
+import org.joml.Matrix4f;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.llamalad7.mixinextras.sugar.Local;
+
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.resource.SynchronousResourceReloader;
 import net.wimods.chestesp.ChestEspMod;
 
 @Mixin(GameRenderer.class)
-public abstract class GameRendererMixin
-	implements AutoCloseable, SynchronousResourceReloader
+public abstract class GameRendererMixin implements AutoCloseable
 {
+	@Unique
 	private boolean cancelNextBobView;
 	
+	/**
+	 * Fires the CameraTransformViewBobbingEvent event and records whether the
+	 * next view-bobbing call should be cancelled.
+	 */
 	@Inject(at = @At(value = "INVOKE",
 		target = "Lnet/minecraft/client/render/GameRenderer;bobView(Lnet/minecraft/client/util/math/MatrixStack;F)V",
-		ordinal = 0),
-		method = "renderWorld(FJLnet/minecraft/client/util/math/MatrixStack;)V")
+		ordinal = 0), method = "renderWorld(FJ)V")
 	private void onRenderWorldViewBobbing(float tickDelta, long limitTime,
-		MatrixStack matrices, CallbackInfo ci)
+		CallbackInfo ci)
 	{
 		ChestEspMod chestEsp = ChestEspMod.getInstance();
 		
@@ -38,6 +44,10 @@ public abstract class GameRendererMixin
 			cancelNextBobView = chestEsp.shouldCancelViewBobbing();
 	}
 	
+	/**
+	 * Cancels the view-bobbing call if requested by the last
+	 * CameraTransformViewBobbingEvent.
+	 */
 	@Inject(at = @At("HEAD"),
 		method = "bobView(Lnet/minecraft/client/util/math/MatrixStack;F)V",
 		cancellable = true)
@@ -51,10 +61,15 @@ public abstract class GameRendererMixin
 		cancelNextBobView = false;
 	}
 	
+	/**
+	 * This mixin is injected into a random method call later in the
+	 * renderWorld() method to ensure that cancelNextBobView is always reset
+	 * after the view-bobbing call.
+	 */
 	@Inject(at = @At("HEAD"),
-		method = "renderHand(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/Camera;F)V")
-	private void renderHand(MatrixStack matrices, Camera camera,
-		float tickDelta, CallbackInfo ci)
+		method = "renderHand(Lnet/minecraft/client/render/Camera;FLorg/joml/Matrix4f;)V")
+	private void onRenderHand(Camera camera, float tickDelta, Matrix4f matrix4f,
+		CallbackInfo ci)
 	{
 		cancelNextBobView = false;
 	}
@@ -64,10 +79,12 @@ public abstract class GameRendererMixin
 			target = "Lnet/minecraft/client/render/GameRenderer;renderHand:Z",
 			opcode = Opcodes.GETFIELD,
 			ordinal = 0),
-		method = "renderWorld(FJLnet/minecraft/client/util/math/MatrixStack;)V")
+		method = "renderWorld(FJ)V")
 	private void onRenderWorld(float partialTicks, long finishTimeNano,
-		MatrixStack matrixStack, CallbackInfo ci)
+		CallbackInfo ci, @Local(ordinal = 1) Matrix4f matrix4f2)
 	{
+		MatrixStack matrixStack = new MatrixStack();
+		matrixStack.multiplyPositionMatrix(matrix4f2);
 		ChestEspMod chestEsp = ChestEspMod.getInstance();
 		
 		if(chestEsp != null && chestEsp.isEnabled())
